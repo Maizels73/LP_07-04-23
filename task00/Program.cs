9d157434123203abdd5b3609c16bede427608162
Михаил <79217582323@ya.ru>
2023-04-13 13:52:33 +0300

    сделали 1 задание в папке таск00 - коммит в корневой папке

--- task00/Program.cs
+++ task00/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
+Console.WriteLine ("INPUT NUM");
+int num = Convert.ToInt32(Console.ReadLine());
+int sg = num * num;
+Console.WriteLine ($"квадрат числа -{sg}");
