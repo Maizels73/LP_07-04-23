4b87a42a749b5ce5a916b2f1d1655cf844652353
Михаил <79217582323@ya.ru>
2023-04-17 12:17:56 +0300

    сделали 2 задание - дни недели

--- task02/Program.cs
+++ task02/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, выдающую дни недели по номеру
+Console.WriteLine ("INPUT NUM_day");
+int num1 = Convert.ToInt32(Console.ReadLine());
+if ((num1 < 1) || (num1 >7)) {  
+    Console.WriteLine ($"ERROR DAY number"); 
+    }
+    else {
+        switch (num1) {
+    case 1:
+    Console.WriteLine ($"Monday"); 
+    break;
+    case 2:
+    Console.WriteLine ($"Tuesday"); 
+    break;
+    case 3:
+    Console.WriteLine ($"Wednesday"); 
+    break;
+    case 4:
+    Console.WriteLine ($"Thursday"); 
+    break;
+    case 5:
+    Console.WriteLine ($"Friday"); 
+    break;
+    case 6:
+    Console.WriteLine ($"Saturday"); 
+    break;
+    case 7:
+    Console.WriteLine ($"Sunday"); 
+    break;
+
+        }
+    }
