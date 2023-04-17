9a98d678544e2a16cb156a23459a245a365f1b0b
Михаил <79217582323@ya.ru>
2023-04-17 11:52:45 +0300

    коммит таск01.cs

--- task01/Program.cs
+++ task01/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine ("INPUT NUM1");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("INPUT NUM1");
+int num2 = Convert.ToInt32(Console.ReadLine());
+int num2_sq = num2 * num2;
+if (num1 == num2_sq) { 
+    Console.WriteLine ($"{num2} * {num2} = {num1}"); 
+    }
+    else {
+    Console.WriteLine ($"{num2} * {num2} <>{num1}");     
+    }
