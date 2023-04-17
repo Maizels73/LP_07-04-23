5470ab420fce74e00ed1c950be5f86d33236e3f2
Михаил <79217582323@ya.ru>
2023-04-17 13:34:56 +0300

    сделали задание 5

--- task03/Program.cs
+++ task03/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine ("INPUT number");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num_start = num1 * -1;
+  for (int i = num_start; i < num1 +1; i++)
+    {
+        Console.Write ($"{i} "); 
+    }
