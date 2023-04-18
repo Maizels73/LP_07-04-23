53634b1f15c92c2976497116b56d1de544a6c075
Михаил <79217582323@ya.ru>
2023-04-18 14:42:56 +0300

    сделали 7 задание

---
+++ task07/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("INPUT NUM");
+int num1 = Convert.ToInt32(Console.ReadLine());
+if (num1 >= 100 && num1 <1000) {
+    Console.WriteLine (num1%8);
+}
